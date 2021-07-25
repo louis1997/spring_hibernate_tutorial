c7fccc54d8d4ce85a72129691a76cc43450fc1df
phl6 <louisli1997@gmail.com>
2021-07-25 14:55:43 +0800

    done Sec4 Spring IOC XML Configuration

--- src/coms/luv2code/springdemo/HelloSpringApp.java
+++ src/coms/luv2code/springdemo/HelloSpringApp.java
@@ -1,14 +1,23 @@
 package coms.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpringApp {
     public static void main(String[] args){
 
+        //Sec4 Spring IOC - XML Configuration
+        //ch23
+
         //load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         //retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
 
         //call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
 
         //close the application context
+        context.close();
     }
 }
